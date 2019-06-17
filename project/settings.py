4db82a7114069036e23613699e0211cb02a065f3
MatGie <matgie9778@gmail.com>
2019-06-17 20:17:17 +0200

    Adding the possibility of  changing the category's position

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u08-3r#p7_*9@fl9(0p(s0zxnk#xmm4u+aaf2qc5xu+_vj9b02'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
